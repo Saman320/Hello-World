47d0c3a52a2700e1f152ffbbe8ece5bc0b97af0a
Saman Siddiqui <samansiddiqui320@gmail.com>
2024-09-23 21:39:47 +0500

    first commit

---
+++ milestone1/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-indigo-600">
+      <div className="bg-white p-10 rounded-lg shadow-lg max-w-md text-center">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">
+         Hello World
+        </h1>
+        <p className="text-lg text-gray-600">
+          Hello World! Start Our Next JS learning journey together.
+        </p>
+      </div>
+    </div>
+  );
+}
